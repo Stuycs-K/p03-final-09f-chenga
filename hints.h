740cad87dbfe9cb208e54123c6994eabbcbed195
Alvin Cheng <acheng60@stuy.edu>
2026-01-09 14:39:03 -0500

    hints h file and setup h file

---
+++ hints.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+
+#ifndef HINTS_H
+#define HINTS_H
+
+int err();
---
+++ setup.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+
+#ifndef SETUP_H
+#define SETUP_H
+
+int err();
+int setup();
