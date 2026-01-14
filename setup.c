68a99bb2583b36ec266d773be86b1fad74c1a235
Alvin Cheng <acheng60@stuy.edu>
2026-01-14 14:39:31 -0500

    created shared memory for highscore tracking, used semaphores for single access, functions: setup() and update()

--- setup.c
+++ setup.c
@@ -1,1 +1,46 @@
+#include "setup.h"
 
+void err(){
+    printf("errno %d\n", errno);
+    printf("%s\n", strerror(errno));
+    exit(1);
+}
+
+void setup(){
+  int *highscore;
+  int shmid;
+  shmid = shmget(KEY, sizeof(int), IPC_CREAT | 0666);
+  if (shmid < 0) err();
+  highscore = shmat(shmid, 0, 0);
+  *highscore = 0;
+  shmdt(highscore);
+
+  int semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0666);
+  if (semd < 0) err();
+  union semun users;
+  users.val = 1;
+  semctl(semd, 0, SETVAL, users);
+}
+void update(int newScore){
+  int semd = semget(KEY, 1, 0);
+  if (semd == -1) err();
+  struct sembuf sb;
+  sb.sem_num = 0;
+  sb.sem_flg = SEM_UNDO;
+  sb.sem_op = -1;
+  if(semop(semd, &sb, 1)==-1) err();
+
+  int shmid = shmget(KEY, sizeof(int), 0);
+  if (shmid == -1) err();
+  int *highscore = shmat(shmid, 0, 0);
+
+  if (newScore > *highscore){
+    *highscore = newScore;
+    printf("NEW HIGHSCORE!: %d", *highscore);
+  }
+  else {
+    printf("Highscore: \n", *highscore);
+  }
+
+  shmdt(highscore);
+}
--- setup.h
+++ setup.h
@@ -5,11 +5,22 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <time.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
 
 #ifndef SETUP_H
 #define SETUP_H
 
+#define KEY 207940560
+union semun {
+int val;
+};
+
 void err();
-int setup();
+void setup();
+void update(int newScore);
+
 
 #endif
